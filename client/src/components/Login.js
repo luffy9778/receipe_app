f7528e1ce66ceb28014a6ac6e7b23a7b795473d2
luffy9778 <luffy97780.gmail.com>
2024-06-08 18:26:11 +0530

    added login register... in cient side

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,42 @@
-
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
 
 const Login = () => {
+  const[username,setUsername]=useState("")
+  const[password,setPassword]=useState("")
+  const handelRegister=async(e)=>{
+    e.preventDefault()
+    try {
+      const result=await axios.post("http://localhost:3000/auth/login",{
+        username,password
+      })
+      console.log(result)
+      alert("login successfull")
+      setUsername("")
+      setPassword("")
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handelRegister}>
+        <input 
+         type='text'
+         placeholder='username'
+         required
+         value={username}
+         onChange={(e)=>setUsername(e.target.value)}/><br/>
+        <input
+          type='password'
+          placeholder='password'
+          required
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}/><br/>
+        <button type='submit'>Log in</button>
+      </form>
+    </div>
   )
 }
 
